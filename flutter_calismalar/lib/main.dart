7dfb86c637c0caafbe7a0708cc61eb5838641e11
Hasan Emre AVARİSLİ <81103889+emreavarisli@users.noreply.github.com>
2022-11-03 20:48:26 +0300

    Ders Notları

--- flutter_calismalar/lib/main.dart
+++ flutter_calismalar/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -14,9 +16,13 @@ import '101/image_learn.dart';
 import '101/indicator_learn.dart';
 import '101/list_tile_learn.dart';
 import '101/padding_learn.dart';
+import '101/page_view_learn.dart';
 import '101/scaffold_learn.dart';
 import '101/stack_learn.dart';
+import '101/statefull_learn.dart';
+import '101/statefull_life_cycle_learn.dart';
 import '101/stateless_learn.dart';
+import '101/text_field_learn.dart';
 import 'demos/note_demos_view.dart';
 import 'demos/stack_demo_view.dart';
 
@@ -36,17 +42,23 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
           progressIndicatorTheme: const ProgressIndicatorThemeData(color: Colors.white),
           listTileTheme: const ListTileThemeData(contentPadding: EdgeInsets.zero),
-          cardTheme: CardTheme(
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
-          ),
+          cardTheme: CardTheme(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))),
           errorColor: ColorsItems.sulu,
+          textSelectionTheme: const TextSelectionThemeData(
+            selectionColor: Colors.red, cursorColor: Colors.green, selectionHandleColor: Colors.black),
+          inputDecorationTheme: const InputDecorationTheme(
+            filled: true, fillColor: Colors.white, iconColor: Colors.red, labelStyle: TextStyle(color: Colors.lime),
+            border: OutlineInputBorder(),
+              floatingLabelStyle: TextStyle(color: Colors.red, fontSize: 24, fontWeight: FontWeight.w600),),
+          textTheme: TextTheme(subtitle1: TextStyle(color: Colors.red)),
           appBarTheme: const AppBarTheme(
           centerTitle: true,
           backgroundColor: Colors.transparent,
           elevation: 0,
           systemOverlayStyle: SystemUiOverlayStyle.light,
       )),
-      home: const StackDemoView(),
+      //home: const StatefullLifeCycleLearn(message: "Velibb"),
+      home: const TextFieldLearn(),
     );
   }
 }
